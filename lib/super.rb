742d69e300068444320fcb11a5e8c5dc4056e936
Marcelo Wiermann <marcelo.wiermann@gmail.com>
2019-06-25 17:18:54 +0200

    feat(Struct): add auto ISO8601 time parsing

--- lib/super.rb
+++ lib/super.rb
@@ -1,10 +1,12 @@
 require 'singleton'
 require 'zeitwerk'
 require 'concurrent'
+require 'time'
 
 require_relative 'super/version'
 require_relative 'super/service'
 require_relative 'super/component'
 require_relative 'super/serializer'
+require_relative 'super/errors'
 
 module Super; end
--- lib/super/struct/attribute.rb
+++ lib/super/struct/attribute.rb
@@ -1,14 +1,46 @@
 module Super
   module Struct
     class Attribute
-      def initialize(options)
-        return if options[:type].nil?
+      def initialize(options = {})
+        @type = options[:type]
+        @decoder = decoder_for(options)
       end
 
       def decode(value)
-        return value if @decoder.nil?
+        return instance_exec(value, &@decoder) if @decoder
+        return unless value
+        raise Super::Errors::DecodeError if @type && !value.is_a?(@type)
 
-        @decoder.call(value)
+        value
+      end
+
+      private
+
+      def decoder_for(options)
+        options[:decoder] ||
+          codec_decoder_for(options[:codec]) ||
+          type_decoder_for(options[:type])
+      end
+
+      def codec_decoder_for(codec = nil)
+        codec && ->(value) { codec.decode(value) }
+      end
+
+      def type_decoder_for(type = nil)
+        type &&
+          support_decoder_for(type) ||
+          native_type_decoder_for(type)
+      end
+
+      def support_decoder_for(type)
+        codec = Kernel.const_get("Super::Codecs::#{type}Codec")&.new
+        return unless codec
+
+        ->(value) { codec.decode(value) }
+      end
+
+      def native_type_decoder_for(type)
+        type.respond_to?(:decode) && ->(value) { type.decode(value) }
       end
     end
   end
