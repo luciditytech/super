d7cf55a5d52c83aae064ed54bd47fbcdb9a3473b
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2019-08-08 17:04:44 -0300

    fix(cache): add redis_cache.rb
    - add new redis cache
    - add performance scripts

---
+++ spec/performance/memory_cache.rb
@@ -0,0 +1,42 @@
+require 'bundler/setup'
+require 'super'
+require 'super/struct'
+require 'super/memory_cache'
+
+require 'securerandom'
+
+require 'benchmark/ips'
+require 'benchmark/memory'
+
+class Klass
+  include Super::Struct
+  attribute :id
+  attribute :payload
+end
+
+cache = Super::MemoryCache.new
+
+data = Array.new(100) do
+  Klass.new(
+    id: SecureRandom.uuid,
+    payload: SecureRandom.hex(1024)
+  )
+end
+
+Benchmark.ips do |x|
+  x.report do
+    10_000.times do
+      entry = data.sample
+      cache.fetch(entry.id) { entry }
+    end
+  end
+end
+
+Benchmark.memory do |x|
+  x.report do
+    10_000.times do
+      entry = data.sample
+      cache.fetch(entry.id) { entry }
+    end
+  end
+end
---
+++ spec/performance/redis_cache.rb
@@ -0,0 +1,48 @@
+require 'bundler/setup'
+require 'super'
+require 'super/struct'
+require 'super/redis_cache'
+
+require 'redis'
+require 'connection_pool'
+require 'securerandom'
+
+require 'benchmark/ips'
+require 'benchmark/memory'
+
+class Klass
+  include Super::Struct
+  attribute :id
+  attribute :payload
+end
+
+cache = Super::RedisCache.new.tap do |config|
+  config.pool = ConnectionPool.new(size: 8) do
+    Redis.new
+  end
+end
+
+data = Array.new(100_000) do
+  Klass.new(
+    id: SecureRandom.uuid,
+    payload: SecureRandom.hex(1024)
+  )
+end
+
+Benchmark.ips do |x|
+  x.report do
+    10_000.times do
+      entry = data.sample
+      cache.fetch(entry.id, ttl: 10) { entry }
+    end
+  end
+end
+
+Benchmark.memory do |x|
+  x.report do
+    10_000.times do
+      entry = data.sample
+      cache.fetch(entry.id, ttl: 10) { entry }
+    end
+  end
+end
